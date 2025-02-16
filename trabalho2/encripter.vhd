c0578a0ede389a9b196d061a2455bca9545918eb
Alexandre <alexandreaanp@gmail.com>
2025-02-16 00:52:24 +0000

    Added reset and clock signal

--- trabalho2/encripter.vhd
+++ trabalho2/encripter.vhd
@@ -5,6 +5,8 @@ use ieee.numeric_std.all;
 entity encripter is
     Port ( 
         data : in std_logic_vector(7 downto 0);
+        clk: in std_logic;
+        rst: in std_logic;
         encripted_data : out std_logic_vector(7 downto 0)
     );
 end encripter;
@@ -12,30 +14,36 @@ end encripter;
 
 architecture Behavioral of encripter is
     signal bit_process : STD_LOGIC;
-    signal updated_data : unsigned(7 downto 0) := unsigned(data);
+    signal updated_data : unsigned(7 downto 0);
 begin
     process (data)
     begin 
-        for i in 0 to 7 loop
+        if rst = '1' then
+            bit_process <= '0';
 
-            -- get the last bit
-            bit_process <= updated_data(0);
+        elsif rising_edge(clk) then
+            updated_data <= unsigned(data);
+            for i in 0 to 7 loop
 
-            -- add to ouput encripted_data vector
-            encripted_data(i) <= bit_process;
+                -- get the last bit
+                bit_process <= updated_data(0);
 
-            -- xor bit_process to the element before the last
-            bit_process <= bit_process xor data(1);
+                -- add to ouput encripted_data vector
+                encripted_data(i) <= bit_process;
 
-            -- shift to right one
-            updated_data <= unsigned(updated_data srl 1);
+                -- xor bit_process to the element before the last
+                bit_process <= bit_process xor data(1);
 
-            -- set the first element with the value of bit_process
-            updated_data(7) <= bit_process;
+                -- shift to right one
+                updated_data <= unsigned(updated_data srl 1);
 
-        end loop;
+                -- set the first element with the value of bit_process
+                updated_data(7) <= bit_process;
 
-        encripted_data <= std_logic_vector(updated_data);
+            end loop;
+            encripted_data <= std_logic_vector(updated_data);
+        end if;
+        
     end process;
 
 
--- trabalho2/tb-encripter.vhd
+++ trabalho2/tb-encripter.vhd
@@ -13,42 +13,73 @@ architecture behavior of tb_encripter is
     component encripter
         Port (
             data : in std_logic_vector(7 downto 0);
+            clk : in std_logic;
+            rst : in std_logic;
             encripted_data : out std_logic_vector(7 downto 0)
         );
     end component;
 
+
     -- Signals to connect to the UUT
-    signal data          : std_logic_vector(7 downto 0) := "10101010"; -- Example input
+    signal data          : std_logic_vector(7 downto 0); -- Example input
+    signal clk           : std_logic := '0';
+    signal rst           : std_logic := '0';
     signal encripted_data : std_logic_vector(7 downto 0);
 
+
+    constant clk_period : time := 10 ns;
 begin
     -- Instantiate the Unit Under Test (UUT)
     uut: encripter port map (
         data => data,
+        clk => clk,
+        rst => rst,
         encripted_data => encripted_data
     );
+
+
+    -- Clock generation
+    clk_process: process
+    begin
+       while true loop
+            clk <= '0';
+            wait for clk_period/2;
+            clk <= '1';
+            wait for clk_period/2;
+        end loop;
+    end process;
 
     -- Stimulus process to drive the inputs and generate a clock
     stim_proc: process
     begin
+
+
+        clk <= '0';
+        rst <= '1';
+        wait for clk_period;
+
+        rst <= '0';
+        wait for clk_period *3;
+
         -- Apply some test vectors
         -- First input
         data <= "11010101";   -- Example data
-        wait for 20 ns;       -- Wait for 10 ns
+        wait for clk_period * 3;       -- Wait for 10 ns
         
         -- Second input
         data <= "10101010";   -- Another test case
-        wait for 20 ns;       -- Wait for 10 ns
+        wait for clk_period * 3;       -- Wait for 10 ns
         
         -- Third input
         data <= "11110000";   -- Another test case
-        wait for 20 ns;       -- Wait for 10 ns
+        wait for clk_period * 3;       -- Wait for 10 ns
         
         -- Fourth input
         data <= "00001111";   -- Another test case
-        wait for 20 ns;       -- Wait for 10 ns
+        wait for clk_period * 3;       -- Wait for 10 ns
 
         -- End the simulation
+        assert false report "End of simulation" severity failure;
         wait;
     end process;
 

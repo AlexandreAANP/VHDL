f95d63eb46f4bdca77f697a0f040a4c6986078d4
Alexandre <alexandreaanp@gmail.com>
2025-04-26 12:39:39 +0100

    Fix encripting

--- trabalho5/controller1.vhd
+++ trabalho5/controller1.vhd
@@ -16,16 +16,18 @@ end controller1;
 architecture Behavioral of controller1 is
     constant ROM_SIZE :integer := 50; -- how many elements rom have
     constant ROM_DATA_SIZE : integer := 16;
-    type state_type is (INIT, READING, SENDING, ENCRIPTING, SPLITING, WAIT_FOR_UART, DONE);
+    type state_type is (INIT, READING, ENCRIPTING, SENDING, DONE);
     signal state : state_type := INIT;
     signal rom_addr : unsigned(5 downto 0);
     signal rom_data_out : signed(ROM_DATA_SIZE-1 downto 0);
+    signal rom_index : integer := 0;
     signal index : integer := 0;
 
     signal cypher_in : std_logic;
     signal cypher_out : std_logic;
     signal cypher_en : std_logic := '0';
     signal encripted_data : std_logic_vector(ROM_DATA_SIZE/2-1 downto 0);
+    signal encripted_index: integer :=0;
     -- signal cypher_rst : std_logic;
     -- signal start_rom_uart : std_logic := '0';
 
@@ -36,6 +38,7 @@ architecture Behavioral of controller1 is
     signal uart_start : std_logic := '0';
     signal uart_rx : std_logic := '1'; -- set '1' for uart doesn't start serial receiving
     signal uart_receiving_in_serial : std_logic := '0'; -- in this case the uart will never receive in serial
+    signal test_index : std_logic;
 
     component controller1_rom is 
         Port(
@@ -99,48 +102,75 @@ begin
 
     start_uart <= uart_start;
     controller_process: process(clk, uart_busy)
-    variable rom_index : integer := 0;
-    variable encripted_index : integer := 0;
+    
+    -- variable encripted_index : integer := 0;
     variable is_first_part : std_logic := '1';
     variable uart_already_start : std_logic := '0';
+    variable save_bit_encripted : std_logic :='0';
     begin
         if rst = '1' then
             state <= READING;
             index <= 0;
-        elsif rising_edge(clk) then
+        elsif falling_edge(clk) then
             case state is
                 when READING =>
-                    rom_addr <= to_unsigned(index, rom_addr'length);
-                    state <= SENDING;
-                    
-                when SENDING =>
-                    cypher_en <= '1'; -- enabling cypher
-                    cypher_in <= rom_data_out(ROM_DATA_SIZE-1-rom_index);
-                    rom_index := rom_index + 1;
-                    if rom_index = ROM_DATA_SIZE then
-                        rom_index := 0;
-                        index <= index + 1;
-                    end if;
-                    state <= ENCRIPTING;
-
-                    
                     if index = ROM_SIZE then
                         state <= DONE;
+                    else
+                        rom_addr <= to_unsigned(index, rom_addr'length);
+                        state <= ENCRIPTING;
+                        cypher_en <= '1';
+                        rom_index <= 1;
+                        encripted_index <= 0;
+                        cypher_in <= rom_data_out(0);
                     end if;
                     
                 when ENCRIPTING =>
-                    if encripted_index = ROM_DATA_SIZE/2 then
-                        uart_in_data <= encripted_data(ROM_DATA_SIZE/2 - 1 downto 0);
-                        uart_start <= '1';
-                        encripted_data(ROM_DATA_SIZE/2 - 1 downto 0) <= (others => '0');
-                        encripted_index := 0; 
-                    else
+                    
+
+                    if encripted_index = 0  then
                         encripted_data(encripted_index) <= cypher_out;
-                        uart_start <= '0';
-                        encripted_index := encripted_index + 1;
+                        cypher_in <= rom_data_out(rom_index);
+                        rom_index <= rom_index +1;
+                        encripted_index <= encripted_index + 1;   
+
+                       
+                        
+                    elsif encripted_index = 7 then
+                        encripted_data(encripted_index) <= cypher_out;
+                        test_index <= cypher_out;
+                        if rom_index < 15 then
+                            cypher_in <= rom_data_out(rom_index);
+                            rom_index <= rom_index +1;
+                        end if;
+                        cypher_en <= '0';
+                        encripted_index <= 0;
+                        
+                        -- send to uart
+                        state <= SENDING;
+                    else 
+                        encripted_data(encripted_index) <= cypher_out;
+                        test_index <= cypher_out;
+                        if rom_index < 16 then
+                            cypher_in <= rom_data_out(rom_index);
+                            rom_index <= rom_index +1;
+                        end if;
+                        encripted_index <= encripted_index +1;
+                        
                     end if;
-                    state <= SENDING; 
-                
+                    
+                when SENDING =>
+                        uart_in_data <= encripted_data;
+                        encripted_data <= (others => 'U');
+
+                        uart_start <= '1';
+                        if rom_index > 15 then
+                            state <= READING;
+                            index <= index +1;
+                        else 
+                            state <= ENCRIPTING;
+                            cypher_en <= '1';
+                        end if;    
                 
                 when others =>
                     assert false report "End of simulation" severity failure;

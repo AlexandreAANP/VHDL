da193aa7d4f5c13c20c135df8c1ad274381376dc
Alexandre <alexandreaanp@gmail.com>
2025-04-29 22:20:23 +0100

    Added new uart for tb

--- trabalho4/tb-uart.vhd
+++ trabalho4/tb-uart.vhd
@@ -11,13 +11,21 @@ architecture testbench of uart_tb is
     signal clk : std_logic := '0';
     signal rst : std_logic := '1';
     signal en : std_logic := '0';
-    signal rx : std_logic := '1';
-    signal data_in : std_logic_vector(0 to data_width_const - 1);
-    signal is_busy : std_logic;
-    signal data_invalid : std_logic;
-    signal tx : std_logic;
-    signal data_out : std_logic_vector(0 to data_width_const - 1);
-    signal start : std_logic := '0'; -- start signal
+    signal rx_1 : std_logic := '1';
+    signal rx_2 : std_logic;
+    signal data_in_1 : std_logic_vector(0 to data_width_const - 1);
+    signal data_in_2 : std_logic_vector(0 to data_width_const -1);
+    signal is_busy_1 : std_logic;
+    signal is_busy_2 : std_logic;
+    signal data_invalid_1 : std_logic;
+    signal data_invalid_2 : std_logic;
+    signal tx_1 : std_logic;
+    signal tx_2 : std_logic;
+    signal data_out_1 : std_logic_vector(0 to data_width_const - 1);
+    signal data_out_2 : std_logic_vector(0 to data_width_const - 1);
+    signal start_1 : std_logic := '0';
+    signal start_2 : std_logic := '0'; 
+    
     
     constant clk_period : time := 10 ns;
     
@@ -41,36 +49,49 @@ architecture testbench of uart_tb is
     
 begin
     -- Instantiate the UART module
-    uut: uart port map (
+    uut_1: uart port map (
         clk => clk,
         rst => rst,
         en => en,
-        rx => rx,
-        start => start, -- Start signal is not used in this testbench
-        data_in => data_in,
-        is_busy => is_busy,
-        data_invalid => data_invalid,
-        tx => tx,
-        data_out => data_out
+        rx => rx_1,
+        start => start_1,
+        data_in => data_in_1,
+        is_busy => is_busy_1,
+        data_invalid => data_invalid_1,
+        tx => tx_1,
+        data_out => data_out_1
     );
+
+    uut_2: uart port map (
+        clk => clk,
+        rst => rst,
+        en => en,
+        rx => tx_1,
+        start => start_2,
+        data_in => data_out_1,
+        is_busy => is_busy_2,
+        data_invalid => data_invalid_2,
+        tx => tx_2,
+        data_out => data_out_2
+    );
+
     
     -- Clock process
     clk_process: process
     begin
-        while now < 1000 ns loop  -- Run simulation for a defined time
+        while true loop  -- Run simulation for a defined time
             clk <= '0';
             wait for clk_period / 2;
             clk <= '1';
             wait for clk_period / 2;
         end loop;
-        wait;
     end process;
     
     -- Stimulus process
     stimulus_process: process
     begin
         -- initialize values
-        data_in <= (others => '0');
+        data_in_1 <= (others => '0');
 
         -- Reset sequence
         rst <= '1';
@@ -79,58 +100,50 @@ begin
         wait for 20 ns;
         
         -- Start receiving data
-        start <= '1'; -- Start signal
-        rx <= '0'; -- Start bit
+        start_1 <= '1'; -- Start signal
+        rx_1 <= '0'; -- Start bit
         wait for clk_period;
-        start <= '0'; -- Reset start signal
+        start_1 <= '0'; -- Reset start signal
         -- Send data bits (example: 8-bit 10101010)
-        rx <= '1'; wait for clk_period;  -- Bit 0
-        rx <= '0'; wait for clk_period;  -- Bit 1
-        rx <= '1'; wait for clk_period;  -- Bit 2
-        rx <= '0'; wait for clk_period;  -- Bit 3
-        rx <= '1'; wait for clk_period;  -- Bit 4
-        rx <= '0'; wait for clk_period;  -- Bit 5
-        rx <= '1'; wait for clk_period;  -- Bit 6
-        rx <= '0'; wait for clk_period;  -- Bit 7
-        rx <= '1'; wait for clk_period;  -- parity it
+        rx_1 <= '1'; wait for clk_period;  -- Bit 0
+        rx_1 <= '0'; wait for clk_period;  -- Bit 1
+        rx_1 <= '1'; wait for clk_period;  -- Bit 2
+        rx_1 <= '0'; wait for clk_period;  -- Bit 3
+        rx_1 <= '1'; wait for clk_period;  -- Bit 4
+        rx_1 <= '0'; wait for clk_period;  -- Bit 5
+        rx_1 <= '1'; wait for clk_period;  -- Bit 6
+        rx_1 <= '0'; wait for clk_period;  -- Bit 7
+        rx_1 <= '1'; wait for clk_period;  -- parity it
         
-        rx <= '1'; -- Stop bit
-        wait for 5*clk_period;
-
-        start <= '1'; -- Start signal
-        rx <= '0'; -- Start bit
+        rx_1 <= '1'; -- Stop bit
+        start_2 <= '1';
         wait for clk_period;
-        start <= '0'; -- Reset start signal
+        start_2 <= '0';
+
+        start_1 <= '1'; -- Start signal
+        rx_1 <= '0'; -- Start bit
+        wait for clk_period;
+        start_1 <= '0'; -- Reset start signal
 
         -- Send data bits (example: 8-bit 10101010)
-        rx <= '1'; wait for clk_period;  -- Bit 0
-        rx <= '0'; wait for clk_period;  -- Bit 1
-        rx <= '1'; wait for clk_period;  -- Bit 2
-        rx <= '0'; wait for clk_period;  -- Bit 3
-        rx <= '1'; wait for clk_period;  -- Bit 4
-        rx <= '0'; wait for clk_period;  -- Bit 5
-        rx <= '1'; wait for clk_period;  -- Bit 6
-        rx <= '0'; wait for clk_period;  -- Bit 7
-        rx <= '0'; wait for clk_period;  -- parity it
+        rx_1 <= '1'; wait for clk_period;  -- Bit 0
+        rx_1 <= '0'; wait for clk_period;  -- Bit 1
+        rx_1 <= '1'; wait for clk_period;  -- Bit 2
+        rx_1 <= '0'; wait for clk_period;  -- Bit 3
+        rx_1 <= '1'; wait for clk_period;  -- Bit 4
+        rx_1 <= '0'; wait for clk_period;  -- Bit 5
+        rx_1 <= '1'; wait for clk_period;  -- Bit 6
+        rx_1 <= '0'; wait for clk_period;  -- Bit 7
+        rx_1 <= '0'; wait for clk_period;  -- parity it
         
-        rx <= '1'; -- Stop bit
-        
+        rx_1 <= '1'; -- Stop bit
+        start_2 <= '1';
+        wait for clk_period;
+        start_2 <= '0';
         -- Wait and observe output
         wait for 100 ns;
 
-        -- send paralel data
-        start <= '1'; -- Start signal
-        data_in <= "11100101"; wait for clk_period;
-        start <= '0'; -- Reset start signal
-        wait for 100 ns;
-        start <= '1'; -- Start signal
-        data_in <= "10101010"; wait for clk_period;
-        start <= '0'; -- Reset start signal
 
-        wait for 100 ns;
-        start <= '1'; -- Start signal
-        data_in <= "11101010"; wait for clk_period;
-        start <= '0'; -- Reset start signal
         wait for 400 ns;
         assert false report "End of simulation" severity failure;
         -- End of simulation

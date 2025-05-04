6e73c7783a6e4bb7a15c844131a77a6d20a4b4f3
Alexandre <alexandreaanp@gmail.com>
2025-05-04 10:43:22 +0100

    Added git ignore

--- trabalho5/report/compile.ps1
+++ trabalho5/report/compile.ps1
@@ -4,14 +4,3 @@ rm .\cr-trab5.toc
 rm .\cr-trab5.out
 pdflatex .\cr-trab5.tex
 pdflatex .\cr-trab5.tex
-
-if ($LASTEXITCODE -eq 0) {
-    # Replace this with the function or command you want to execute
-    #DELETE ALL LOG FILES
-    # rm .\cr-trab5.log
-    # rm .\cr-trab5.aux
-    # rm .\cr-trab5.toc
-    # rm .\cr-trab5.out
-} else {
-    Write-Output "The program failed to run."
-}
